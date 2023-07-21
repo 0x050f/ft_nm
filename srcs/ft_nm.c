62367fb7fc2c57210997d0e3c9b2436b3514cf62
0x050f <47715948+0x050f@users.noreply.github.com>
2023-07-21 16:55:59 +0200

    Check using elf header

--- srcs/ft_nm.c
+++ srcs/ft_nm.c
@@ -1,10 +1,22 @@
 #include "ft_nm.h"
 
-int			check_file(void *addr) {
-	uint8_t magic[4] = {ELFMAG0, ELFMAG1, ELFMAG2, ELFMAG3};
+int			check_file(void *addr, size_t size) {
+	Elf64_Ehdr *header;
 
-	if (memcmp(addr, magic, sizeof(magic)))
+	if (size < sizeof(Elf64_Ehdr))
 		return (-ERR_FORMAT);
+	header = addr;
+	if (header->e_ident[EI_MAG0] != ELFMAG0 ||
+		header->e_ident[EI_MAG1] != ELFMAG1 ||
+		header->e_ident[EI_MAG2] != ELFMAG2 ||
+		header->e_ident[EI_MAG3] != ELFMAG3)
+		return (-ERR_FORMAT);
+	if (header->e_ident[EI_CLASS] == ELFCLASSNONE)
+		return (-ERR_FORMAT);
+	if (header->e_ident[EI_CLASS] == ELFCLASS32) {
+	}
+	if (header->e_ident[EI_CLASS] == ELFCLASS64) {
+	}
 	return (0);
 }
 
@@ -34,7 +46,7 @@ int			process_file(char *filename) {
 	addr = mmap(NULL, statbuf.st_size, PROT_READ, MAP_SHARED, fd, 0);
 	if (addr == MAP_FAILED)
 		return (-ERR_ERRNO);
-	ret = check_file(addr);
+	ret = check_file(addr, statbuf.st_size);
 	if (ret < 0)
 		return (ret);
 	if (munmap(addr, statbuf.st_size) == -1)
